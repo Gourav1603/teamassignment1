ec339e18733830b27576728ef9751061acf19b93
c0655617 <c0655617@SB-N208-08.lambton.on.ca>
2015-05-15 10:00:00 -0400

    Basic for Assignment

---
+++ src/teamassignment1/queue.java
@@ -0,0 +1,27 @@
+package teamassignment1;
+
+
+import java.util.ArrayDeque;
+import java.util.Date;
+import java.util.Queue;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author c0655617
+ */
+public class queue {
+   
+    Queue<order> orderQueue = new ArrayDeque<>();
+    
+    public void add(order ordr) {
+        orderQueue.add(ordr);
+        ordr.setTimeReceived(new Date());
+    }
+}
+
