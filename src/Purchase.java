1a7f911aab2e4f4b663218f36e27267d423916c9
c0655617 <c0655617@SB-N208-08.lambton.on.ca>
2015-05-15 10:08:14 -0400

    Basic for Assignment

--- src/Purchase.java
+++ src/Purchase.java
@@ -1,4 +1,4 @@
-package teamassignment1;
+
 
 /*
  * To change this license header, choose License Headers in Project Properties.
--- src/order.java
+++ src/order.java
@@ -1,4 +1,4 @@
-package teamassignment1;
+
 
 
 import java.util.ArrayList;
